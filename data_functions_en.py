74f3f67dc5e257cc95a0d764327ab1f1e2d4496a
Michal Kaukič <mike@feelmath.eu>
2024-01-20 18:16:00 +0100

    substantially reworked *py

--- data_functions_en.py
+++ data_functions_en.py
@@ -3,43 +3,62 @@ import plotly.express as px
 import os, pickle
 
 
-def passengers_data(frm, pick=True, height=400, what=['passengers', 'rides', 'fare_hours']):
-    column = 'pick_dt' if pick else 'drop_dt'
-    ylabel = 'Pickups' if pick else 'Dropoffs'
-    by_hours = frm.group_by(frm[column].dt.hour()) # pick_hour or drop_hour
-    pgs = by_hours.agg(pl.col('passengers').sum())
-    pgs = pgs.with_columns(by_hours.agg(pl.col('fare').count().alias('rides').cast(pl.Float32)))
-    pgs = pgs.with_columns(by_hours.agg(pl.col('fare').sum().alias('fare_hours')))    
-    max_jaz, max_pas, max_far = pgs['rides'].max(), pgs['passengers'].max(), pgs['fare_hours'].max()
-    coeff = max(max_jaz, max_pas) / max_far
-    pgs = pgs.with_columns(pl.col('fare_hours') * coeff)
-    trzba = pgs['fare_hours'].sum()
-    scalestr = f"Fare multiplied by {coeff:.3f}, total fare {trzba:.2f}" if 'fare_hours' in what else ''
+def get_totals(frm, pick=True):
+    columns = ['pick_day','pick_hour'] if pick else ['drop_day','drop_hour']
+    df_total = frm.group_by(columns)\
+                            .agg([pl.col('fare').sum().alias('total_fare'),\
+                                  pl.col('passengers').sum().alias('total_pass'),
+                                  pl.col('fare').count().alias('total_rides')])\
+                            .sort(by=columns)
+    return df_total
+
+def get_monthly(frm, pick=True):
+    column = 'pick_day' if pick else 'drop_day'
     
-    graf = px.bar(pgs, x=column, y=what, barmode='group', height=height, width=750, title=scalestr, 
-                  labels={column: 'hours', 'value': ylabel})
+
+def total_graphs(frm, pick=True, height=400, what=['total_pass', 'total_rides', 'total_fare'], monthly=False):
+    if not monthly:
+        frm = get_totals(frm) if pick else get_totals(frm, pick=False)
+        column = 'pick_hour' if pick else 'drop_hour'
+    else: 
+        column = 'pick_day' if pick else 'drop_day'
+    ylabel = 'Pickups' if pick else 'Dropoffs' 
+    max_jaz, max_pas, max_far = frm['total_rides'].max(), frm['total_pass'].max(), frm['total_fare'].max()
+    coeff = max(max_jaz, max_pas) / max_far
+    trzba = frm['total_fare'].sum()
+    frm = frm.with_columns(pl.col('total_fare') * coeff)
+    scalestr = f"Fare multiplied by {coeff:.3f}, total fare {trzba:.2f}" if ('total_fare' in what) else ''
+    colname = 'Hour' if not monthly else 'Day'
+    graf = px.bar(frm, x=column, y=what, barmode='group', height=height, width=750, title=scalestr, 
+                  labels={column: colname, 'value': ylabel})
     return graf
 
 
 # We will make plots only once, then load them from pickle file
-def make_graphs(df):
-    plotfile = 'data/dfdays_en.pic'
-    if not os.path.isfile(plotfile):
+def make_graphs(df, create=True):
+    plotfile = 'data/dfdays.pic'
+    if create:
         dfdays = {}
         for day in range(1, 32):
-            pick_df = df.filter(pl.col('pick_dt').dt.day() == day) 
-            drop_df = df.filter(pl.col('drop_dt').dt.day() == day)
+            pick_df = df.filter(pl.col('pick_day') == day) 
+            drop_df = df.filter(pl.col('drop_day') == day)
             dfdays[day] = {}
-            dfdays[day]['pick_graph'] = passengers_data(pick_df) 
-            dfdays[day]['drop_graph'] = passengers_data(drop_df, pick=False)
+            dfdays[day]['pick_graph'] = total_graphs(pick_df) 
+            dfdays[day]['drop_graph'] = total_graphs(drop_df, pick=False)
             pickle.dump(dfdays, open(plotfile,'wb'))
     else:
         dfdays = pickle.load(open(plotfile,'rb'))
     return dfdays
 
+def monthly_frame(frm, pick=True):
+    column = 'pick_day' if pick else 'drop_day'
+    df_month = frm.group_by(column).agg([pl.col('fare').sum().alias('total_fare'), 
+                                         pl.col('passengers').sum().alias('total_pass'),
+                                         pl.col('fare').count().alias('total_rides')]).sort(by=column)
+    return df_month
 
 # Plots - pickups and dropoffs by hour (rides, passenger count, fare), total
 def static_graphs(frm):
-    pick_bar = passengers_data(frm, height=300)
-    drop_bar = passengers_data(frm, pick=False, height=300)
+    pick_bar = total_graphs(monthly_frame(frm), height=350, monthly=True)
+    drop_bar = total_graphs(monthly_frame(frm, pick=False), height=350, monthly=True, pick=False)
     return pick_bar, drop_bar 
--- taxi_marimo_en.py
+++ taxi_marimo_en.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.1.78"
+__generated_with = "0.1.79"
 app = marimo.App(width="full")
 
 
@@ -15,13 +15,13 @@ def __():
     import polars as pl
     import plotly.express as px
     import os
-    from data_functions_en import make_graphs, static_graphs, passengers_data
-    return make_graphs, os, passengers_data, pl, px, static_graphs
+    from data_functions_en import make_graphs, static_graphs, total_graphs
+    return make_graphs, os, pl, px, static_graphs, total_graphs
 
 
 @app.cell
 def __(make_graphs, os, pl):
-    nsample = 600000
+    nsample = 155000
     _dflocal = 'data/nyc_taxi.parq'
     _exists_local = os.path.isfile(_dflocal)
     _dfname = _dflocal if _exists_local else 'https://feelmath.eu/Download/nyc_taxi.parq'
@@ -29,7 +29,12 @@ def __(make_graphs, os, pl):
     if not _exists_local:
         df.write_parq(_dflocal)
     meanloc = [df['pick_lat'].mean(), df['pick_lon'].mean()]
-    dfdays = make_graphs(df)
+    df = df.with_columns([(df['pick_dt'].dt.day().alias('pick_day')), 
+                          (df['pick_dt'].dt.hour().alias('pick_hour')),
+                          (df['drop_dt'].dt.day().alias('drop_day')),
+                          (df['drop_dt'].dt.hour().alias('drop_hour'))])\
+                        .drop(['pick_dt', 'drop_dt'])
+    dfdays = make_graphs(df, create=True)
     return df, dfdays, meanloc, nsample
 
 
@@ -63,9 +68,10 @@ def __(day_choose, dfdays, direction, mo):
 def __(day_choose, df, direction, hour_choose, meanloc, mo, px):
     def view_map():
         is_pick = (direction.value == 'Pickup')
-        col_dt = 'pick_dt' if is_pick else 'drop_dt'
-        df_filtered = df.filter((df[col_dt].dt.day() == day_choose.value) & 
-                                (df[col_dt].dt.hour() == hour_choose.value))
+        col_day = 'pick_day' if is_pick else 'drop_day'
+        col_hour = 'pick_hour' if is_pick else 'drop_hour'
+        df_filtered = df.filter((df[col_day] == day_choose.value) & 
+                                (df[col_hour] == hour_choose.value))
         col_lat = 'pick_lat' if is_pick else 'drop_lat'
         col_lon = 'pick_lon' if is_pick else 'drop_lon'
         lat, lon = df_filtered[col_lat], df_filtered[col_lon]
@@ -75,9 +81,9 @@ def __(day_choose, df, direction, hour_choose, meanloc, mo, px):
                                 mapbox_style="open-street-map", zoom=10, width=750, height=500)
         fig.update_traces(marker={"size": 4})
         fig.update_layout(margin={'t': 25}, hovermode=False)
-        return mo.ui.plotly(fig)
-    mapplot = view_map()
-    return mapplot, view_map
+        return mo.ui.plotly(fig), df_filtered
+    mapplot, df_filtered = view_map()
+    return df_filtered, mapplot, view_map
 
 
 @app.cell
@@ -91,9 +97,9 @@ def __(
     mapplot,
     mo,
     nsample,
-    passengers_data,
     pl,
     static_plots,
+    total_graphs,
 ):
     _main_title = mo.md(
         f"""
@@ -108,14 +114,14 @@ def __(
         lon_max, lat_min = _mapranges[1]
         df_ranges = df.filter((lat_min < pl.col('pick_lat')) & (pl.col('pick_lat') < lat_max) &
                               (lon_min < pl.col('pick_lon')) & (pl.col('pick_lon') < lon_max) &
-                              (pl.col('pick_dt').dt.day() == day_choose.value))
+                              (pl.col('pick_day') == map_day_choose.value))
 
-    _local_plot = passengers_data(df_ranges, pick=True, what=['passengers', 'rides']) if _local_data else mo.md('# No selection')
+    _local_plot = total_graphs(df_ranges, pick=True, what=['total_pass', 'total_rides']) if _local_data else mo.md('# No selection')
     _selection_body = mo.vstack([map_day_choose, _local_plot]) if _local_data else mo.vstack([_local_plot])
 
     _maps = mo.vstack([mo.hstack([direction, day_choose, hour_choose], justify='center'), 
                        mapplot], align='center')
-    _tabs = mo.tabs({'Day plots': hourly, 'Summary plots': static_plots,
+    _tabs = mo.tabs({'Day plots': hourly, 'Monthly plots': static_plots,
                      'Locations on map': _maps, 'Selection graphs': _selection_body})
 
     app_tabs = mo.vstack([_main_title, _tabs], align='stretch') 
